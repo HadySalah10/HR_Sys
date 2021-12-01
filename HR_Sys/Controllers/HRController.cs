a2c1d3fea6e9673aafe66e977117e242d889d77f
HadySalah10 <47247169+HadySalah10@users.noreply.github.com>
2021-12-01 21:47:24 +0200

    add first controller HR

---
+++ HR_Sys/Controllers/HRController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HR_Sys.Controllers
+{
+    public class HRController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
