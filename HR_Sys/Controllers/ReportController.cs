98077f9b30b17a0ca108a2536ca8fcae7bcf46bc
Manar166 <manar_abdelhameed16@yahoo.com>
2021-12-06 17:22:58 +0200

    add constructor

--- HR_Sys/Controllers/ReportController.cs
+++ HR_Sys/Controllers/ReportController.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using HR_Sys.Models;
 
 namespace HR_Sys.Controllers
 {
     public class ReportController : Controller
     {
+        HrDBContext db;
+        public ReportController(HrDBContext db)
+        {
+            this.db = db;
+
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(db.EmpReports.ToList());
         }
     }
 }
