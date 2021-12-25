8f85e0cc25b132e56c3d08aa873bdb4e83c55efe
Manar166 <manar_abdelhameed16@yahoo.com>
2021-12-25 22:03:50 +0200

    create اجازه عارضه

---
+++ HR_Sys/Controllers/EmployeeVacationController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HR_Sys.ViewModels;
+using HR_Sys.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HR_Sys.Controllers
+{
+    public class EmployeeVacationController : Controller
+    {
+
+        HrDBContext db;
+        public EmployeeVacationController(HrDBContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(db.TypesOfVacationsEmps.ToList());
+        }
+
+        public IActionResult create()
+        {
+            ViewBag.emps = new SelectList(db.Employees.ToList(), "id", "empName");
+            ViewBag.day = new SelectList(db.Days.ToList(), "id", "daysName");
+
+            return View();
+        
+        }
+
+        [HttpPost]
+        public IActionResult create(TypesOfVactionViewModel vacation)
+        {
+            if (ModelState.IsValid)
+            {
+                var dayName = vacation.date.Value.DayOfWeek.ToString();
+                int idDay = db.Days.Where(n => n.displayNameEnglish == dayName).Select(n => n.id).FirstOrDefault();
+                //var emp = db.Employees.Where(n => n.empName == vacation.empName).SingleOrDefault();
+                TypesOfVacationsEmp EmployeeVacation = new TypesOfVacationsEmp();
+                EmployeeVacation.date = vacation.date;
+                EmployeeVacation.empId = vacation.empId;
+                EmployeeVacation.vacId = vacation.vacId;
+                EmployeeVacation.idDays = idDay;
+                
+                
+
+
+                db.TypesOfVacationsEmps.Add(EmployeeVacation);
+                db.SaveChanges();       
+               
+            }
+            return RedirectToAction("Index");
+
+
+        }
+
+
+
+    }
+}
---
+++ HR_Sys/ViewModels/TypesOfVactionViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using HR_Sys.Models;
+using HR_Sys.Models.BaseIDEntity;
+
+
+namespace HR_Sys.ViewModels
+{
+    public class TypesOfVactionViewModel
+    {
+        public int? id { get; set; }
+
+        [ForeignKey("emp")]
+        public int empId { get; set; }
+
+        [ForeignKey("VacationType")]
+        public int vacId { get; set; }
+        [Required (ErrorMessage = "هذا الحقل مطلوب")]
+        public DateTime? date { get; set; }
+        [ForeignKey("days")]
+        public int idDays { get; set; }
+
+        //object from employee
+        //public virtual Employee emp { get; set; }
+
+        ////object from vacation
+        //public virtual VacationType VacationType { get; set; }
+        //// OBJECT FROM  days
+        //public virtual Days days { get; set; }
+
+    }
+}
